a6e1eb1e0e2ce249766b5f920aff29dc0c6464c8
Dennis Todoric <todoricdennis@gmx.de>
2021-01-14 13:00:49 +0100

    init

---
+++ skyjo-component/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="scoreboard" class="col scoreboard">
+    <div class="scoreboard-head">Scoreboard</div>
+    <div v-for="p in scoreboardPlayers" class="scoreboard-player" :key="p">
+      {{ p.name }} {{ p.points }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "skyjo-scoreboard",
+  data: () => ({
+    scoreboardPlayers: []
+  })
+};
+</script>
+
+<style lang="scss">
+.scoreboard {
+    font-size: 24px;
+    margin-top: 15px;
+}
+.scoreboard-head {
+    /*text-decoration: underline;*/
+    margin-bottom: 10px;
+    width: 100%;
+    border-bottom: 2px solid black;
+}
+.scoreboard-player {
+    padding: 0;
+    margin: 0;
+}
+</style>
