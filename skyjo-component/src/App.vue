56364283772824afb3a595836e5a2145f52595d6
Dennis Todoric <todoricdennis@gmx.de>
2021-01-14 13:18:23 +0100

    init

--- skyjo-component/src/App.vue
+++ skyjo-component/src/App.vue
@@ -18,17 +18,17 @@ export default {
 
 <style lang="scss">
 .scoreboard {
-    font-size: 24px;
-    margin-top: 15px;
+  font-size: 24px;
+  margin-top: 15px;
 }
 .scoreboard-head {
-    /*text-decoration: underline;*/
-    margin-bottom: 10px;
-    width: 100%;
-    border-bottom: 2px solid black;
+  /*text-decoration: underline;*/
+  margin-bottom: 10px;
+  width: 100%;
+  border-bottom: 2px solid black;
 }
 .scoreboard-player {
-    padding: 0;
-    margin: 0;
+  padding: 0;
+  margin: 0;
 }
 </style>
