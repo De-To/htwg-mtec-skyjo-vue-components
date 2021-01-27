0e5f1f787bc342ed6d765f1ba55cb3a35cba76aa
Dennis Todoric <todoricdennis@gmx.de>
2021-01-27 13:16:23 +0100

    vue componente erstellt

--- skyjo-component/src/main.js
+++ skyjo-component/src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
-import App from "./App.vue";
+import Scoreboard from "./skyjo-scoreboard.vue";
+import Gamedeck from "./skyjo-gamedeck.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(Scoreboard)
+}).$mount("#skyjo-scoreboard");
+
+new Vue({
+  render: h => h(Gamedeck)
+}).$mount("#skyjo-gamedeck");
---
+++ skyjo-component/src/skyjo-gamedeck.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row head-table">
+    <div id="trade" v-for="d in dpv" class="col deckAndPile" :key="d">{{ d }}</div>
+    <div id="draw" class="col deckAndPile">#</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "skyjo-gamedeck",
+  data: () => ({
+    dpv: []
+  })
+};
+</script>
+
+<style lang="scss">
+.head-table {
+  border: 2px solid black;
+  vertical-align: middle;
+  text-align: center;
+  display: flex;
+  margin-top: 15px;
+  max-width: 150px;
+  max-height: 29px;
+}
+.deckAndPile:hover {
+  background-color: black;
+  color: white;
+  cursor: pointer;
+}
+</style>
